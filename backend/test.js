0b118efa736d0794f1f2e4baf30e47cc2a0b2ce0
NithinAmaraneni <158589682+NithinAmaraneni@users.noreply.github.com>
2025-03-04 00:04:24 +0530

    updated 1.0

--- backend/test.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const enteredPassword = "user123";
-const storedHash = "$2b$10$KFi6Yx.W5A5QA7gTS8G8w.6/OpJc20AWhm8uJyL5Nfm6Q3Rg9yEiG";
-
-bcrypt.compare(enteredPassword, storedHash).then(console.log);
